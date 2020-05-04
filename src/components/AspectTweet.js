1607d4f76cf93bb6682c32814a28f1915ad7fc2a
Gurpreet Singh <gurpreet.singh@lapr195.successivetech.com>
2020-05-04 10:20:13 +0530

    Worked on highlighted text for aspect tweet list

--- src/components/AspectTweet.js
+++ src/components/AspectTweet.js
@@ -6,6 +6,7 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import CustomEmoji from './CustomEmoji';
 import Typography from '@material-ui/core/Typography';
 import { cyan } from '@material-ui/core/colors';
+import Highlighter from "react-highlight-words";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,19 +19,28 @@ const useStyles = makeStyles((theme) => ({
   },
   aspect:{
     overflowWrap: "break-word",
-    color: "cyan"
+  },
+  highlight:{
+    backgroundColor: 'cyan'
   }
 }));
 
 const Tweet = (props) => {
   const classes = useStyles();
+  const highlightedText = <Highlighter
+  unhighlightClassName={classes.inline}
+  highlightClassName={classes.highlight}
+  searchWords={props.aspect}
+  autoEscape={true}
+  textToHighlight={props.text}
+/>
   return (<ListItem 
     button
   >
     <ListItemAvatar>
       <CustomEmoji polarity={props.sentiment} />
     </ListItemAvatar>
-    <ListItemText primary={props.text} secondary={
+    <ListItemText primary={highlightedText} secondary={
             <React.Fragment>
               <span className={classes.aspect}>Aspect: {props.aspect.join(',')}</span>
               <Typography
